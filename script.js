02980bd03cc3c5b3f57b960f346570e26d76d645
DevScissors <sean.scissors@gmail.com>
2026-04-23 18:23:45 -0700

    TOP-calculator: 95% finished with the styling and overall lok and feel

--- script.js
+++ script.js
@@ -14,12 +14,24 @@ function operate(operator, num1, num2) {
     }
 }
 
-function generateButtons() {
-    const calcDiv = document.querySelector(".calculator-container");
-    const operatorButton = document.createElement("button");
-    operatorButton.textContent = "+";
-    console.log(calcDiv);
-    calcDiv.appendChild(operatorButton);
+function clearButtonToggle(displayValue) {
+    const clearButton = document.querySelector(".clear");
+    console.log(clearButton);
+    const deleteLeftIcon = document.createElement("img");
+    deleteLeftIcon.src = "./images/delete-left-icon.png";
+    if (!displayValue) {
+        return clearButton.textContent = "AC";
+    } else {
+        return clearButton.appendChild(deleteLeftIcon);
+    }
 }
 
-generateButtons();
+clearButtonToggle(false);
+
+// function generateButtons() {
+//     const calcDiv = document.querySelector(".calculator-container");
+//     const operatorButton = document.createElement("button");
+//     operatorButton.textContent = "+";
+//     console.log(calcDiv);
+//     calcDiv.appendChild(operatorButton);
+// }
